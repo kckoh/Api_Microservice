933778e95dcc90734a97e7f1096ef6e4d223a015
kckoh <kyungcheolkoh@gmail.com>
2019-09-13 14:12:55 +0000

    time server

--- node/index.js
+++ node/index.js
@@ -2,17 +2,23 @@ var express = require('express');
 var app = express();
 require('dotenv').config()
 
-app.get('/', function (req, res) {
-   res.send("Hello World")
+//middleware practice
+middleware = (req,res, next) => {
+    console.log(req.method, req.path, req.ip)
+    next()
+}
 
+app.get('/', middleware, function(req,res){
+    res.send("HelloWorld")
 });
 
-app.get('/json', function (req, res) {
+
+//read json and using .env practice
+app.get('/json', middleware, function (req, res) {
     var hello = "hello json"
     var message = {"message": hello}
     if(process.env.MESSAGE_STYLE == "uppercase"){
         message.message = message.message.toUpperCase()
-        console.log(hello)
         res.json(message)
     }
     else{
@@ -20,6 +26,14 @@ app.get('/json', function (req, res) {
     }
 
 });
+
+// Chain Middleware to Create a Time Server
+app.get('/now', function(req, res, next) {
+req.time= new Date().toString()
+next();
+}, function(req, res) {
+res.json({time:req.time});
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
