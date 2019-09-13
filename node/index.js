34957c94303234dad0628794c09c6de1566c2c2b
kckoh <kyungcheolkoh@gmail.com>
2019-09-13 13:35:35 +0000

    using environment variable

--- node/index.js
+++ node/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 app.get('/', function (req, res) {
+   res.send("Hello World")
 
+});
+
+app.get('/json', function (req, res) {
+    var hello = "hello json"
+    var message = {"message": hello}
     if(process.env.MESSAGE_STYLE == "uppercase"){
-        res.send("its working")
+        message.message = message.message.toUpperCase()
+        console.log(hello)
+        res.json(message)
     }
     else{
         res.json({"message": "Hello World"})
